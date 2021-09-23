22ef66871039b5a5dbae5e49a8239feb3c6058a2
goldenxp <sunder@goldenxp.com>
2021-09-23 10:17:33 -0700

    Trigger for Collision Trigger Events

---
+++ goldenxp/STAC/Conditions/ColliderTagCondition.cs
@@ -0,0 +1,25 @@
+﻿namespace goldenxp
+{
+	using UnityEngine;
+	using UnityEngine.Events;
+	
+	[AddComponentMenu("STAC/Conditions/Collider Tag Condition")]
+	public class ColliderTagCondition : SimpleConditionBase
+	{
+		public Collider colliderToTest;
+		public string tagToMatch;
+		
+		public override bool Check()
+		{
+			if (!string.IsNullOrEmpty(tagToMatch) && colliderToTest.tag == tagToMatch)
+				return true;
+			
+			return false;
+		}
+		
+		public void SetTestCollider(Collider col)
+		{
+			colliderToTest = col;
+		}
+	}
+}
---
+++ goldenxp/STAC/Triggers/TriggerEventsTrigger.cs
@@ -0,0 +1,43 @@
+﻿namespace goldenxp
+{
+	using UnityEngine;
+	using UnityEngine.InputSystem;
+	
+	[AddComponentMenu("STAC/Triggers/Trigger (Collider) Trigger")]
+	public class TriggerEventsTrigger : SimpleTriggerBase
+	{
+		public SimpleActionBase actionEnter;
+		public SimpleActionBase actionExit;
+		public SimpleActionBase actionStay;
+		public ColliderTagCondition condition;
+		
+		protected void OnTriggerEnter(Collider other)
+		{
+			if (CheckColliderCondition(other))
+				actionEnter.Run();
+		}
+		
+		protected void OnTriggerExit(Collider other)
+		{
+			if (CheckColliderCondition(other))
+				actionEnter.Run();
+		}
+		
+		protected void OnTriggerStay(Collider other)
+		{
+			if (CheckColliderCondition(other))
+				actionEnter.Run();
+		}
+		
+		protected bool CheckColliderCondition(Collider collider)
+		{
+			if (condition != null)
+			{
+				condition.SetTestCollider(collider);
+				return condition.Check();
+			}
+			
+			return true;
+		}
+	}
+}
