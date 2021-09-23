212c3d908f5cecaab58952f9aad9ce060692acdc
goldenxp <sunder@goldenxp.com>
2021-09-23 10:23:26 -0700

    Remove TriggerAction actions where parity is currently met

--- TriggerAction/Actions/ActionBase.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class ActionBase : MonoBehaviour {
-    public virtual void Act () {
-        Debug.Log("Override ActionBase#Act!");
-    }
-}
