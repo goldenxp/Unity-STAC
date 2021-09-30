74525a4276189b3a53ddb8b2155b1a6a54eb2886
goldenxp <sunder@goldenxp.com>
2021-09-30 16:43:27 -0700

    Play Audio added, Play Sound removed

---
+++ goldenxp/STAC/Actions/PlayAudioAction.cs
@@ -0,0 +1,25 @@
+﻿namespace goldenxp
+{
+	using UnityEngine;
+	using UnityEngine.Events;
+	
+	[AddComponentMenu("STAC/Actions/Play Audio Action")]
+	[HelpURL("https://github.com/goldenxp/Unity-STAC/wiki/PlayAudioAction")]
+	public class PlayAudioAction : SimpleActionBase
+	{
+		[Tooltip("Audio Source that will be played.")]
+		public AudioSource source;
+		[Tooltip("If provided, the Action randomly picks a clip from the list for the Audio Source.")]
+		public AudioClip[] clips;
+		
+		public override void Run()
+		{
+			if (clips != null && clips.Length > 0)
+			{
+				source.clip = clips[Random.Range(0, clips.Length)];
+			}
+			
+			source.Play();
+		}
+	}
+}
