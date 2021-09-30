52f5258d31a70ba27dceded1d796c6b8de6dd58c
goldenxp <sunder@goldenxp.com>
2021-09-30 16:21:07 -0700

    Instantiate Action added, Spawn Prefab Action removed

---
+++ goldenxp/STAC/Actions/InstantiateAction.cs
@@ -0,0 +1,25 @@
+﻿namespace goldenxp
+{
+	using UnityEngine;
+	using UnityEngine.Events;
+	
+	[AddComponentMenu("STAC/Actions/Instantiate Action")]
+	[HelpURL("https://github.com/goldenxp/Unity-STAC/wiki/InstantiateAction")]
+	public class InstantiateAction : SimpleActionBase
+	{
+		[Tooltip("GameObject that will be cloned.")]
+		public GameObject prefab;
+		
+		[Tooltip("Time after which instantiated object is destroyed, only destroys if greater than zero")]
+		public float destroyTime = 0f;
+		
+		public override void Run()
+		{
+			GameObject instance = Instantiate(prefab, transform.position, transform.rotation);
+			if (destroyTime > 0)
+			{
+				Destroy(instance, destroyTime);
+			}
+		}
+	}
+}
