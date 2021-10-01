d2ec9bfc89eb9bc8dc526deb3f1d5ed9545ccd1a
goldenxp <sunder@goldenxp.com>
2021-09-30 20:48:48 -0700

    Add InCooldownCondition, Remove CooldownAction

---
+++ goldenxp/STAC/Conditions/InCooldownCondition.cs
@@ -0,0 +1,31 @@
+﻿namespace goldenxp
+{
+	using System.Collections;
+	using UnityEngine;
+	
+	[AddComponentMenu("STAC/Conditions/In Cooldown Condition")]
+	[HelpURL(Utils.HelpURL + "InCooldownCondition")]
+	public class InCooldownCondition : SimpleConditionBase
+	{
+		public float cooldownTime;
+		public bool coolingDown = false;
+		
+		public override bool Check()
+		{
+			if (!coolingDown)
+			{
+				coolingDown = true;
+				StartCoroutine(ResetCooldown());
+				return false;
+			}
+			
+			return true;
+		}
+		
+		IEnumerator ResetCooldown()
+		{
+			yield return new WaitForSeconds(cooldownTime);
+			coolingDown = false;
+		}
+	}
+}
