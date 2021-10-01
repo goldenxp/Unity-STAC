5a778eca1f55fbf7dfe8fae7340e97784f734ea2
goldenxp <sunder@goldenxp.com>
2021-09-30 19:34:42 -0700

    Remove OnceAction in lieu of PassOnceCondition
    Separating the run-once logic into a condition allows for better reuse and further decouples action state from what the action actually does.

---
+++ goldenxp/STAC/Conditions/PassOnceCondition.cs
@@ -0,0 +1,23 @@
+﻿namespace goldenxp
+{
+	using UnityEngine;
+	using UnityEngine.Events;
+	
+	[AddComponentMenu("STAC/Conditions/Pass Once Condition")]
+	[HelpURL(Utils.HelpURL + "PassOnceCondition")]
+	public class PassOnceCondition : SimpleConditionBase
+	{
+		private bool passed = false;
+		
+		public override bool Check()
+		{
+			if (!passed)
+			{
+				passed = true;
+				return true;
+			}
+			
+			return false;
+		}
+	}
+}
