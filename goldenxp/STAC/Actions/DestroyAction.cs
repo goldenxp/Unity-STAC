c49c8fc0e1e467022fe66b79938869c03dd65a6b
goldenxp <sunder@goldenxp.com>
2021-10-19 13:31:04 -0700

    Adding: Destroy Action
    This replaces TriggerActions: RemoveGameObject and RemoveGameObjects

---
+++ goldenxp/STAC/Actions/DestroyAction.cs
@@ -0,0 +1,21 @@
+﻿namespace goldenxp
+{
+	using UnityEngine;
+	using UnityEngine.Events;
+	
+	[AddComponentMenu("STAC/Actions/Destroy Action")]
+	[HelpURL(Utils.HelpURL + "DestroyAction")]
+	public class DestroyAction : SimpleActionBase
+	{
+		[Tooltip("GameObject(s) that will be destroyed.")]
+		public GameObject[] gameObjects;
+		
+		public override void Run()
+		{
+			foreach (GameObject item in gameObjects)
+			{
+				Destroy(item);
+			}
+		}
+	}
+}
