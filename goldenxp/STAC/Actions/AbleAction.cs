506adf445a965517512134e7879d8013ae6b0358
goldenxp <sunder@goldenxp.com>
2021-10-19 14:07:08 -0700

    Adding: AbleAction
    Not the best name but it's for enabling and disabling game objects - a toggle effect is also possible.

---
+++ goldenxp/STAC/Actions/AbleAction.cs
@@ -0,0 +1,28 @@
+﻿namespace goldenxp
+{
+	using UnityEngine;
+	using UnityEngine.Events;
+	
+	[AddComponentMenu("STAC/Actions/Able Action")]
+	[HelpURL(Utils.HelpURL + "AbleAction")]
+	public class AbleAction : SimpleActionBase
+	{
+		[Tooltip("GameObject(s) that will be enabled or disabled.")]
+		public GameObject[] gameObjects;
+		[Tooltip("Whether to enable or disable the GameObject(s)")]
+		public bool state;
+		[Tooltip("If true, it will flip the state after each run. This makes toggling possible.")]
+		public bool flipState;
+		
+		public override void Run()
+		{
+			foreach (GameObject item in gameObjects)
+			{
+				item.SetActive(state);
+			}
+			
+			if (flipState)
+				state = !state;
+		}
+	}
+}
